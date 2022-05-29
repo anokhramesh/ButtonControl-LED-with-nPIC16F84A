cefe7bcb48a8019d5da61eff36ec16def331997a
Anokhautomation <67797819+anokhramesh@users.noreply.github.com>
2022-05-29 20:15:04 +0400

    Add files via upload

---
+++ ButtoncontrollLED.c
@@ -0,0 +1,19 @@
+void main() {
+
+
+    TRISB.F0 = 0;  //   Set B0 pin as Output
+    TRISA.F0 = 1;  // set A0 pin as Inputt
+    while(1)
+    {
+    if(PORTA.F0 == 1)
+    {
+    PORTB.F0 = 1;  // led turn ON
+    }
+    else
+    {
+    PORTB.F0 = 0;  // led turn OFF
+    }
+
+    }
+
+}
